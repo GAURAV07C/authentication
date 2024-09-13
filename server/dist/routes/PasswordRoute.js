c1f599b477e0652a33e69e6728763e02d61e6e6e
Gaurav Kumar <gaurav989267@gmail.com>
2024-09-13 21:34:19 +0530

    jh

---
+++ server/dist/routes/PasswordRoute.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { Password } from "../controllers/PasswordController.js";
+import { authLimiter } from "../config/rateLimit.js";
+const router = Router();
+const passwordController = new Password();
+router
+    .route("/forget-password")
+    .post(authLimiter, passwordController.forgotPassword);
+router
+    .route("/reset-password")
+    .post(authLimiter, passwordController.resetPassword);
+export default router;
